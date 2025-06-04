e91a93329b58b0c766a6a5e40d341a87351c0419
Yutman <kuriapeter95@gmail.com>
2025-06-05 01:50:30 +0300

    Modify layout.tsx to integrate MobileNav with responsive design

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
-import SideBar from "@/components/SideBar";
+import Sidebar from "@/components/SideBar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
+
 
 export default function RootLayout({
   children,
@@ -8,10 +11,18 @@ export default function RootLayout({
   const loggedIn = {firstName: 'Muchiri', lastName: 'Kuria'};
 
   return (
-   <main className="flex h-screen w-full font-inter">
-    <SideBar 
-        user={loggedIn}/>
-    {children}
-   </main>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
   );
 }
