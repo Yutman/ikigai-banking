9e8336055ac5a770cad83b1bf452a24455eecfb9
Yutman <kuriapeter95@gmail.com>
2025-06-04 22:19:17 +0300

    Add HeaderBox component for title and greeting display

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -1,8 +1,7 @@
-const HeaderBox = ({ type = "title", title, subtext, user }: HeaderBoxProps) => {
+const HeaderBox = ({type='title', title, subtext, user}: HeaderBoxProps) => {
   return (
     <div className="header-box">
-      <h1 className="header-box-title">
-        {title}
+      <h1 className="header-box-title">{title}
         {type === 'greeting' && (
           <span className="text-bankGradient">
             &nbsp;{user}
