9d06c76a5f9dcd322cf7553a711472764bbce983
Yutman <kuriapeter95@gmail.com>
2025-06-17 20:28:37 +0300

    style: format Image component for improved readability

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -20,7 +20,11 @@ export default async function RootLayout({
 
       <div className="flex size-full flex-col">
         <div className="root-layout">
-          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <Image 
+              src="/icons/logo.svg" 
+              width={30} 
+              height={30} 
+              alt="logo" />
           <div>
             <MobileNav user={loggedIn} />
           </div>
