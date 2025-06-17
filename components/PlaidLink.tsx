1a111477de198cfc8c74de542d6cd7310974b43a
Yutman <kuriapeter95@gmail.com>
2025-06-18 02:44:37 +0300

    refactor: clean up imports in PlaidLink component for better readability

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,10 +1,9 @@
-import React, { use } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './ui/button'
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
 import { useRouter } from 'next/navigation';
-import Image from 'next/image'
-import {useState, useCallback, useEffect} from 'react'
-import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions'
+import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions';
+import Image from 'next/image';
 
 const PlaidLink = ({user, variant}: PlaidLinkProps) => {
     const router = useRouter();
